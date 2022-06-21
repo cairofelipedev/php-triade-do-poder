e771f95d6dff3b0ad555e4b42725c01ddde12798
Cairo Felipe Developer <cairofelipedev@gmail.com>
2022-06-21 11:58:42 -0300

    Ultimate settings

--- index.php
+++ index.php
@@ -6,17 +6,30 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="./assets/css/style.css" rel="stylesheet">
   <script src="https://cdn.tailwindcss.com"></script>
+  <script>
+    tailwind.config = {
+      theme: {
+        extend: {
+          colors: {
+            puff: '#FDDCC3',
+            champ: '#F4CF88',
+            atomic: '#F69358',
+            kobe: '#843412',
+            mandarin: '#DB7745',
+            xike: '#111125',
+          }
+        }
+      }
+    }
+  </script>
 </head>
 
 <body>
 
-  <main class="relative overflow-hidden h-screen">
+  <main>
     <div class="hero flex relative z-20 items-center overflow-hidden">
-      <div class="container mx-auto px-6 flex relative py-16">
+      <div class="container mx-auto px-6 flex relative">
         <div class="sm:w-2/3 lg:w-2/5 flex flex-col relative z-20">
-          <div class="md:hidden sm:block">
-            <img src="./assets/img/foto1.png" class="w-full" />
-          </div>
           <!-- <h1 class="text-center text-9xl font-bold flex flex-col text-transparent bg-clip-text bg-gradient-to-r from-yellow-400 to-orange-600">
             <span class="text-5xl sm:text-6xl">
               Tríade
@@ -26,22 +39,56 @@
             </span>
             Poder
           </h1> -->
-          <img src="./assets/img/triade-do-poder.png" class="w-full" />
-          <div class="mt-8">
-            <form class="p-12">
-              <div class="flex items-center text-lg mb-6 md:mb-8">
-                <input type="text" id="username" class="bg-gray-200 pl-4 py-2 md:py-4 focus:outline-none w-full" placeholder="Username" />
+          <img src="./assets/img/triade-do-poder.png" />
+          <div>
+            <form action="send-email.php" method="POST">
+              <div class="flex items-center text-lg mb-3 md:mb-4">
+                <input type="text" id="name" name="name" class="placeholder-mandarin bg-puff pl-4 py-2 rounded-xl focus:outline-none w-full" placeholder="Insira seu nome" required />
               </div>
-              <div class="flex items-center text-lg mb-6 md:mb-8">
-                <input type="password" id="password" class="bg-gray-200 pl-4 py-2 md:py-4 focus:outline-none w-full" placeholder="Password" />
+              <div class="flex items-center text-lg mb-3 md:mb-8">
+                <input type="email" id="email" name="email" class="placeholder-mandarin bg-puff pl-4 py-2 rounded-xl focus:outline-none w-full" placeholder="Seu melhor email" required />
               </div>
-              <button class="bg-gradient-to-b from-gray-700 to-gray-900 font-medium p-2 md:p-4 text-white uppercase w-full">Login</button>
+              <button type="submit" class="bg-gradient-to-r from-champ to-atomic font-medium p-2 rounded-xl md:p-3 text-kobe uppercase w-full">QUERO ME INSCREVER AGORA</button>
             </form>
+          </div>
+          <div class="md:hidden sm:block">
+            <img src="./assets/img/foto1.png" class="w-full" />
           </div>
         </div>
         <div class="hidden sm:block sm:w-1/3 lg:w-3/5 relative">
           <img src="./assets/img/foto1.png" class="w-full" />
         </div>
+      </div>
+    </div>
+    <div class="hero2 items-center">
+      <div class="pt-16 ml-auto px-6 lg:w-4/6">
+        <h1 class="text-justify md:text-4xl text-2xl font-semibold text-puff md:mr-32 pb-3">Você gostaria de aumentar o seu faturamento com estratégias simples e validadas?</h1>
+        <p class="text-white pb-3 font-semibold">No evento a Triade do Sucesso, você vai aprender comigo, t udo que é necessário para você se tornumar uma referência no que faz e conquistar os resultados que merece.</p>
+        <p class="text-white pb-3 font-semibold">Será um evento online e gratuito no dia 04 de julho ás 21:00h, onde euvou revelar tudo o que eu fiz para multiplicar meu faturamento em 10 vezes, melhor minha estratégia e posicionamento.</p>
+        <p class="text-white pb-3 font-semibold">Eu vou entregar de graça o que normalmente profissionais da área entregam em cursos de mais de mil reais, e você não pode ficar de fora!</p>
+      </div>
+      <div class="container ml-auto px-6 lg:w-4/6">
+        <form class="lg:w-1/2" action="send-email2.php" method="POST">
+          <div class="flex items-center text-lg mb-3">
+            <input type="email" id="email" name="email" class="placeholder-mandarin bg-puff pl-4 py-2 rounded-xl focus:outline-none w-full" placeholder="Seu melhor email" required />
+          </div>
+          <button type="submit" class="bg-gradient-to-r from-champ to-atomic font-medium p-2 rounded-xl mb-4 md:p-3 text-kobe uppercase w-full">QUERO ME INSCREVER AGORA</button>
+        </form>
+      </div>
+    </div>
+    <img src="./assets/img/Landing-page_03.gif" class="w-full" />
+    <div class="md:p-12 bg-xike">
+      <div class="flex w-full bg-gradient-to-r from-champ to-atomic rounded-xl p-8">
+        <div class="w-1/2">
+          <h1 class="text-center md:text-3xl font-semibold text-kobe">SE INSCREVA AGORA E NÃO PERCA NADA!</h1>
+          <h1 class="text-center md:text-6xl font-semibold text-kobe">SEG 04 JUL 21:00H</h1>
+        </div>
+        <form class="w-1/2" action="send-email2.php" method="POST">
+          <div class="flex items-center text-lg mb-3">
+            <input type="email" id="email" name="email" class="placeholder-mandarin bg-puff pl-4 py-2 rounded-xl focus:outline-none w-full" placeholder="Seu melhor email" required />
+          </div>
+          <button type="submit" class="bg-gradient-to-r from-champ to-atomic font-medium p-2 rounded-xl mb-4 md:p-3 text-kobe uppercase w-full">QUERO ME INSCREVER AGORA</button>
+        </form>
       </div>
     </div>
   </main>
---
+++ send-email2.php
@@ -0,0 +1,58 @@
+<?php
+//Variáveis
+$email = $_POST['email'];
+$data_envio = date('d/m/Y');
+$hora_envio = date('H:i:s');
+
+$arquivo = "
+  <style type='text/css'>
+  body {
+  margin:0px;
+  font-family:Verdane;
+  font-size:12px;
+  color: black;
+  }
+  a{
+  color: black;
+  text-decoration: none;
+  }
+  a:hover {
+  color: #FF0000;
+  text-decoration: none;
+  }
+  </style>
+    <html>
+        <table width='510' border='1' cellpadding='1' cellspacing='1' bgcolor='#FFFFFFFF'>
+                <tr>
+                  <td width='320'>E-mail:<b>$email</b></td>
+                 </tr>
+                </td>
+                </tr>
+                <tr>
+                    <td>Este e-mail foi enviado em <b>$data_envio</b>/<b>$hora_envio</b></td>
+                </tr>
+        </table>
+    </html>
+  ";
+
+//enviar
+
+// emails para quem será enviado o formulário
+$emailenviar = "triadedopoder@cairo.dev.br";
+$destino = $emailenviar;
+$assunto = "Lead do Site";
+
+// É necessário indicar que o formato do e-mail é html
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= "From: Sites cairofelipedev <cairofelipedev@gmail.com>";
+//$headers .= "Bcc: $EmailPadrao\r\n";
+
+$enviaremail = mail($destino, $assunto, $arquivo, $headers);
+if ($enviaremail) {
+  echo ("<script type= 'text/javascript'>alert('Obrigado! Em breve entramos em contato com você');</script>
+            <script>window.location = 'index.php';</script>");
+} else {
+  echo ("<script type= 'text/javascript'>alert('Erro ao enviar! Tente novamente');</script>
+            <script>window.location = 'index.php';</script>");
+}
