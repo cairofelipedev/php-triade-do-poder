35c534a8ed6ad3baeff8eb18a7f97691718e1290
Cairo <cairofelipedev@gmail.com>
2022-07-04 16:08:38 -0300

    New page

---
+++ mvp-estetica-marketing-vendas-e-pocisionamento.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Tríade do Poder</title>
+  <meta name="description" content="Você gostaria de aumentar o seu faturamento com estratégias simples e validadas?" />
+  <meta content="Tríade do Poder" name="keywords">
+  <meta property="og:title" content="Tríade do Poder" />
+  <meta property="og:url" content="https://gabrielapazesteta.com.br/" />
+  <meta property="og:image" content="https://gabrielapazesteta.com.br/assets/img/triade-do-poder.png" />
+
+  <!-- Favicons -->
+  <link href="./assets/img/triade-do-poder.png" rel="icon">
+  <link href="./assets/img/triade-do-poder.png" rel="apple-touch-icon">
+  <link href="./assets/css/style.css" rel="stylesheet">
+  <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+  <main>
+    <div class="md:block hidden">
+      <img src="./assets/img/img1-desk.jpg" />
+      <div class="flex justify-center">
+        <button type="submit" class="btn-mvp w-1/2">
+          QUERO AGORA
+        </button>
+      </div>
+    </div>
+    <div class="md:hidden block">
+      <img src="./assets/img/img1-mob.jpg" />
+      <div class="flex justify-center">
+        <button type="submit" class="btn-mvp w-1/2">
+          QUERO AGORA
+        </button>
+      </div>
+    </div>
+  </main>
+</body>
+
+</html>
